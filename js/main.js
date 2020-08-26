c43f294e7be79cd9f198a2b2e93bcfc74e7f271c
Jarvis <rumayor.javier@gmail.com>
2020-08-26 03:21:29 -0500

    Arranca proyecto con boilerplate 8.0

---
+++ js/main.js
@@ -0,0 +1,15 @@
+const nav = document.querySelector('.bar');
+const topToNav = nav.offsetTop;
+const main = document.querySelector('body');
+
+function stickyNav() {
+    if (window.scrollY >= topToNav) {
+        main.style.paddingTop = nav.offsetHeight + 'px';
+        nav.classList.add('fixed');
+    } else {
+        main.style.paddingTop = 0;
+        nav.classList.remove('fixed');
+    }
+}
+
+window.addEventListener('scroll', stickyNav);
